72e6b99399935d49053f77545ce2812a1148fc82
jacobgonzales <jagonz@live.com>
2023-06-09 13:56:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your code here
+// const formData = {
+//     dogName: "Billy",
+//     dogBreed: "Doberman",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+
+function submitData(name = "jacob", email = "blank@email.com") {
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({
+            name,
+            email
+        }),
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        const p = document.createElement('p')
+        p.innerHTML = data['id']
+        document.body.append(p)
+    })
+    .catch(function(error){
+        const div = document.createElement('div')
+        div.innerHTML = error
+        document.body.append(div)
+    });
+};
